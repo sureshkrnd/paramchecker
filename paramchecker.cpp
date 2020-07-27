91ddae69fae0db8cd9b3fbd3219a9c8cfced0b21
sureshkrnd <68851037+sureshkrnd@users.noreply.github.com>
2020-07-27 17:19:23 +0530

    Update paramchecker.cpp
    Splitted the conditions

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,10 +1,14 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
+  if(bpm < 70) {
+    return false;
+  } else if(bpm > 150) {
     return false;
   } else if(spo2 < 80) {
     return false;
-  } else if(respRate < 30 || respRate > 60) {
+  } else if(respRate < 30) {
+    return false;
+  } else if(respRate > 60){
     return false;
   }
   return true;
