d4849857ee2b0e5252ccbc893ffc36175397574f
sureshkrnd <68851037+sureshkrnd@users.noreply.github.com>
2020-08-03 17:17:10 +0530

    Update paramchecker.cpp
    vehicle_data

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -2,18 +2,37 @@
 #include <vector>
 #include <iostream>
 
-IVitalCheck* vitalCheckers[] = {
-  [bpm] = new VitalRangeCheck(70, 150),
-  [spo2] = new VitalRangeCheck(80, 100),
-  [respRate] = new VitalRangeCheck(30, 60),
-  [avgECG] = new VitalValueCheck(0),
-};
+// IVitalCheck* vitalCheckers[] = {
+//   [bpm] = new VitalRangeCheck(70, 150),
+//   [spo2] = new VitalRangeCheck(80, 100),
+//   [respRate] = new VitalRangeCheck(30, 60),
+//   [avgECG] = new VitalValueCheck(0),
+// };
 
-std::vector<bool> vitalsAreOk(const std::vector<Measurement>& measurements) {
-  std::vector<bool> results;
-  for(auto t = measurements.begin(); t != measurements.end(); t++) {
-    bool vitalResult = vitalCheckers[t->id]->measurementIsOk(t->measured_value);
-    std::cout << "Vital-check result is " << vitalResult << std::endl;
+// std::vector<bool> vitalsAreOk(const std::vector<Measurement>& measurements) {
+//   std::vector<bool> results;
+//   for(auto t = measurements.begin(); t != measurements.end(); t++) {
+//     bool vitalResult = vitalCheckers[t->id]->measurementIsOk(t->measured_value);
+//     std::cout << "Vital-check result is " << vitalResult << std::endl;
+//   }
+//   return results;
+// }
+
+inventory get_vehicle_measurements( telematics input )
+{
+  inventory data;
+  data.vehicle_id = input.vehicle_id;
+  if( input.type == measure::motor_temp )
+  {
+    data.motor_temp = imput.measurement;
   }
-  return results;
+  else if( input.type == measure::battery_pc )
+  {
+    data.battery_pc = imput.measurement;
+  }
+  else( input.type == measure::battery_temp )
+  {
+    data.battery_temp = imput.measurement;
+  }
+  return data;
 }
